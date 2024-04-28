9acc52413143616eb725008145f81f6084af8673
Denis Buchkov <den-domovoi@ya.ru>
2024-04-28 13:47:37 +0300

    init commit

---
+++ app.vue
@@ -0,0 +1,115 @@
+<script setup>
+const description
+    = 'Исследуйтем мир веб технологий вместе. Маленький блог о все что может пригодится разработчику.'
+const ogTitle
+    = 'Исследуйтем мир веб технологий вместе. Маленький блог о все что может пригодится разработчику.'
+const twitterDescription
+    = 'Исследуйтем мир веб технологий вместе. Маленький блог о все что может пригодится разработчику.'
+const image
+    = 'https://res.cloudinary.com/dmecmyphj/image/upload/v1673548905/nuxt-blog/cover_ntgs6u.webp'
+const keywords = 'WEB, Front-end, Back-end, Innovation, Learn, Explore'
+const mySite = 'https://denibu.com/'
+
+useHead({
+  htmlAttrs: {
+    lang: 'ru',
+  },
+  meta: [
+    {
+      name: 'description',
+      content: description,
+    },
+    {
+      name: 'keywords',
+      content: keywords,
+    },
+    // Test on: https://developers.facebook.com/tools/debug/ or https://socialsharepreview.com/
+    {
+      property: 'og:site_name',
+      content: mySite,
+    },
+    {
+      property: 'og:type',
+      content: 'website',
+    },
+    {
+      property: 'og:url',
+      content: mySite,
+    },
+    {
+      property: 'og:title',
+      content: ogTitle,
+    },
+    {
+      property: 'og:description',
+      content: description,
+    },
+    {
+      property: 'og:image',
+      content: image,
+    },
+    // Test on: https://cards-dev.twitter.com/validator or https://socialsharepreview.com/
+    {
+      name: 'twitter:site',
+      content: '',
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    },
+    {
+      name: 'twitter:url',
+      content: mySite,
+    },
+    {
+      name: 'twitter:title',
+      content: ogTitle,
+    },
+    {
+      name: 'twitter:description',
+      content: twitterDescription,
+    },
+    {
+      name: 'twitter:image',
+      content: image,
+    },
+  ],
+})
+</script>
+
+<template>
+  <div class="bg-[#F1F2F4] dark:text-zinc-300 dark:bg-slate-950">
+    <NuxtLoadingIndicator :throttle="0" />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<style>
+  .page-enter-active,
+  .page-leave-active {
+    transition: all 0.4s;
+  }
+
+  .page-enter-from,
+  .page-leave-to {
+    opacity: 0;
+    filter: blur(1rem);
+  }
+
+  .layout-enter-active,
+  .layout-leave-active {
+    transition: all 0.4s;
+  }
+
+  .layout-enter-from,
+  .layout-leave-to {
+    opacity: 0;
+    filter: blur(1rem);
+  }
+
+  html.dark {
+    color-scheme: dark;
+  }
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,85 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+// eslint-disable-next-line n/prefer-global/process
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES'
+  ? {
+      generate: {
+        routes: '/d-blog/',
+      },
+    }
+  : {}
+
+export default defineNuxtConfig({
+  ...routerBase,
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=device-width,initial-scale=1',
+      title: 'D-Blog',
+      titleTemplate: '%s - D-Blog',
+      meta: [
+        {
+          name: 'description',
+          content:
+            'D-Blog Исследуйтем мир веб технологий вместе. Маленький блог о всем что может пригодится разработчику.',
+        },
+      ],
+    },
+    pageTransition: {
+      name: 'page',
+      mode: 'out-in',
+    },
+    layoutTransition: {
+      name: 'layout',
+      mode: 'out-in',
+    },
+  },
+
+  sitemap: {
+    strictNuxtContentPaths: true,
+  },
+
+  site: {
+    url: 'https://d-blog.com/',
+    identity: {
+      type: 'Person',
+    },
+    twitter: '',
+  },
+
+  typescript: {
+    strict: true,
+  },
+
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ['/'],
+    },
+  },
+
+  colorMode: {
+    classSuffix: '',
+    preference: 'dark',
+    fallback: 'light',
+  },
+
+  modules: [
+    'nuxt-icon',
+    '@nuxt/image',
+    '@vueuse/nuxt',
+    'nuxt-og-image',
+    '@nuxt/content',
+    '@nuxtjs/robots',
+    '@nuxtjs/fontaine',
+    '@nuxtjs/color-mode',
+    'nuxt-simple-sitemap',
+    '@nuxtjs/tailwindcss',
+  ],
+
+  content: {
+    highlight: {
+      theme: 'dracula',
+    },
+  },
+})
